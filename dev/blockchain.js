934416a30bde2ed15fcefee7f41562ff264c7ed3
Your Name <you@example.com>
2021-05-21 13:22:51 +0200

    adding screenshots

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,41 +1,63 @@
 /* main blockchain code */
 
-
 /*main object of the blockchain */
-function Blockchain () {
-
-    this.chain=[];
-    this.newTransactions=[];
-
+function Blockchain() {
+  this.chain = [];
+  this.pendingTransactions = [];
 }
 
 //method for creating new block and adding it to blckhn
 
-Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
-    //new block object
-    const newBlock = {
-        index: this.chain.length +1,
-        timestamp: Date.now(),
-        transactions: this.newTransactions,
-        nonce: nonce,
-        hash: hash,
-        previousBlockHash: previousBlockHash
-    };
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  //new block object
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
 
-    //setting newTransactions array to null 
-    this.newTransactions = [];
+  //setting pendingTransactions array to null
+  this.pendingTransactions = [];
 
-    //adding block to chain
-    this.chain.push(newBlock);
+  //adding block to chain
+  this.chain.push(newBlock);
 
-    return newBlock;
-}
+  return newBlock;
+};
 
 //getting the last block method
-Blockchain.prototype.getLastBlock= function () {
-    //indexing the last element of the array
-    return this.chain[this.chain.length-1];
-}
+Blockchain.prototype.getLastBlock = function () {
+  //indexing the last element of the array
+  return this.chain[this.chain.length - 1];
+};
 
+/*method for creating new transaction that accepts 3 parameters:
+        1. amount
+        2. sender
+        3. recipient
+
+*/
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  //newTransaction object that will be added to transactions array
+  const newTransaction = {
+    amount,
+    sender,
+    recipient,
+  };
+
+  this.pendingTransactions.push(newTransaction);
+  return this.getLastBlock()["index"] + 1;
+};
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,14 +1,15 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require("./blockchain");
 
 const dukatoni = new Blockchain();
 
-dukatoni.createNewBlock(420, 'ASDFGHJKLY', '1q2w3e4r5t');
-dukatoni.createNewBlock(420, 'QWERTZUIOP', '0o9i8u7z6t');
-dukatoni.createNewBlock(420, 'YXCVBNMASD', '1234567891');
+dukatoni.createNewBlock(420, "ASDFGHJKLY", "1q2w3e4r5t");
+dukatoni.createNewTransaction(100, "ALEXHT845SJ5TKCJ2", "JENN5BG5DF6HT8NG9");
+// dukatoni.createNewBlock(420, "QWERTZUIOP", "0o9i8u7z6t");
+// dukatoni.createNewBlock(420, "YXCVBNMASD", "1234567891");
 
 //testing createNewBlock method
 console.log(dukatoni);
 
 //testing getLastBlock method
-console.log('this is the last element of the blockchain');
-console.log(dukatoni.getLastBlock())
+// console.log("this is the last element of the blockchain");
+// console.log(dukatoni.getLastBlock());
