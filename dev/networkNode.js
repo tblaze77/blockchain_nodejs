f7818f8839213f4d8ebf7d969028b97eed8b9e17
tblaze77 <toni.blazevic77@gmail.com>
2021-05-30 18:25:28 +0200

    testing first post request

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,5 +1,12 @@
 var express = require("express");
 var app = express();
+const bodyParser = require ('body-parser');
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.get("/blockchain", (req, res) => {
   res.send("blockchain endpoint");
@@ -13,4 +20,12 @@ app.get("/mine", (req, res) => {
   res.send("mine endpoint");
 });
 
-app.listen(8000);
+app.post("/transaction", (req,res) => {
+  console.log(req.body);
+  res.send(`the amount of the transaction is ${req.body.amount} dukatoni's`);
+})
+
+app.listen(8000, (err) => {
+  if(err) console.log('error in starting server')
+  else console.log('server is listening on port 8000');
+});
